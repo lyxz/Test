8718adeaf4f913207a52afd2a2006c24e6ff432e
appleLu <luguosui0@163.com>
2015-02-27 12:04:59 +0800

    1

--- lyxz/src/main/java/Test.java
+++ lyxz/src/main/java/Test.java
@@ -2,5 +2,8 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
+		System.out.println("Hello world111!!!!!!!");
 	}
 }
